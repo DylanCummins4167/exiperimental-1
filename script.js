46346ee7eb94d6aea7c6dbafe12cb4373eb56baa
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-11 04:28:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,146 +1,84 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-var specialSymbols =
-  [
-    '@',
-    '%',
-    '+',
-    '\\',
-    '/',
-    "'",
-    '!',
-    '#',
-    '$',
-    '^',
-    '?',
-    ':',
-    ',',
-    ')',
-    '(',
-    '}',
-    '{',
-    ']',
-    '[',
-    '~',
-    '-',
-    '_',
-    '.',
-  ]
-
-var lowerCase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+const questions = [
+	{
+		question: "What is the capital of France?",
+		options: ["Paris", "London", "Berlin", "Madrid"],
+		answer: "Paris"
+	},
+	{
+		question: "What is the largest continent?",
+		options: ["Africa", "Asia", "Europe", "North America"],
+		answer: "Asia"
+	},
+	{
+		question: "What is the currency of Japan?",
+		options: ["Yuan", "Dollar", "Euro", "Yen"],
+		answer: "Yen"
+	},
+	{
+		question: "What is the tallest mammal?",
+		options: ["Elephant", "Giraffe", "Horse", "Lion"],
+		answer: "Giraffe"
+	},
+	{
+		question: "What is the largest planet in our solar system?",
+		options: ["Venus", "Saturn", "Jupiter", "Neptune"],
+		answer: "Jupiter"
+	}
 ];
 
-var upperCase = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
+const container = document.querySelector(".container");
+const timer = document.querySelector("#timer");
+const startBtn = document.querySelector("#start");
+const questionDiv = document.querySelector("#question");
+const optionsDiv = document.querySelector("#options");
 
-function randomCharacter(characterList) {
-  var randomIndex = Math.floor(Math.random() * characterList.length);
+let currentQuestion = 0;
+let score = 0;
+let time = 60;
+let timerInterval;
 
-  return characterList[randomIndex];
+function startQuiz() {
+    startBtn.style.display = "none";
+    timerInterval = setInterval(updateTimer, 1000);
+    updateQuestion();
 }
 
-// Write password to the #password input
-function writePassword() {
-  var randomPassword = '';
+startBtn.addEventListener("click", startQuiz);
 
-  var length = prompt("How many characters long do you want your password to be?");
-
-  // Check if they typed in a valid number
-  if (isNaN(length)) {
-    alert("Enter a valid number.")
-    return;
-  }
-
-  // Convert to a number
-  length = Number(length);
-
-  // Ask which types the user wants for their password
-  var wantNumbers = confirm("Click OK to add numbers to your password.");
-  var wantLowerCases = confirm("Click OK to add lower case letters to your password.");
-  var wantUpperCases = confirm("Click OK to add upper case letters to your password.");
-  var wantSymbols = confirm("Click OK to add special characters to your password.");
-
-  // Array to store wanted characters
-  var charactersList = [];
-
-  // Add the wanted types to the charactersList
-  if (wantNumbers === true) {
-    charactersList = charactersList.concat(numbers);
-  }
-
-  if (wantLowerCases === true) {
-    charactersList = charactersList.concat(lowerCase);
-  }
-  if (wantUpperCases=== true) {
-    charactersList = charactersList.concat(upperCase);
-  }
-  if (wantSymbols === true) {
-    charactersList = charactersList.concat(specialSymbols);
-  }
-
-  for(var i = 0; i < length; i++) {
-    randomPassword += randomCharacter(charactersList)
-  }
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = randomPassword;
+function updateTimer() {
+    time--;
+    timer.textContent = time;
+    if (time === 0) {
+        endQuiz("Time is Up");
+    }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function updateQuestion() {
+    questionDiv.textContent = questions[currentQuestion].question;
+    optionsDiv.innerHTML = "";
+    questions[currentQuestion].options.forEach(option => {
+        const optionDiv = document.createElement("div");
+        optionDiv.classList.add("option");
+        optionDiv.textContent = option;
+        optionDiv.addEventListener("click", () => {
+            if (option === questions[currentQuestion].answer) {
+                score++;
+                currentQuestion++;
+                if (currentQuestion === questions.length) {
+                    endQuiz("Thank You for Your Submission");
+                } else {
+                    updateQuestion();
+                }
+            } else {
+                time -= 10;
+            }
+        });
+        optionsDiv.appendChild(optionDiv);
+    });
+}
+
+function endQuiz(message) {
+    clearInterval(timerInterval);
+    questionDiv.textContent = message;
+    optionsDiv.innerHTML = `<p>Your score is ${score} out of ${questions.length}</p><button onclick="location.reload()">Restart</button>`;
+}
