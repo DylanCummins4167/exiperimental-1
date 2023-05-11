ddbd2e27beb2bd6791ae01988dcd98a2146fc9c5
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-11 04:29:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ const timer = document.querySelector("#timer");
 const startBtn = document.querySelector("#start");
 const questionDiv = document.querySelector("#question");
 const optionsDiv = document.querySelector("#options");
+const scoreboardForm = document.querySelector("#scoreboard-form");
+const scoreboardTable = document.querySelector("#scoreboard-table");
+const initialsInput = document.querySelector("#initials-input");
 
 let currentQuestion = 0;
 let score = 0;
@@ -80,5 +83,31 @@ function updateQuestion() {
 function endQuiz(message) {
     clearInterval(timerInterval);
     questionDiv.textContent = message;
-    optionsDiv.innerHTML = `<p>Your score is ${score} out of ${questions.length}</p><button onclick="location.reload()">Restart</button>`;
+    optionsDiv.innerHTML = `
+        <p>Your score is ${score} out of ${questions.length}</p>
+        <form id="scoreboard-form">
+            <label for="initials-input">Enter your initials:</label>
+            <input type="text" id="initials-input" required>
+            <button type="submit">Submit</button>
+        </form>
+    `;
+    scoreboardForm.addEventListener("submit", submitScore);
 }
+
+function submitScore(event) {
+    event.preventDefault();
+    const initials = initialsInput.value;
+    const scores = JSON.parse(localStorage.getItem("scores")) || [];
+    scores.push({ initials, score });
+    localStorage.setItem("scores", JSON.stringify(scores));
+    showScoreboard(scores);
+}
+
+function showScoreboard(scores) {
+    scoreboardTable.innerHTML = `
+        <thead>
+            <tr>
+                <th>Initials</th>
+                <th>Score</th>
+            </tr>
+        </thead>
